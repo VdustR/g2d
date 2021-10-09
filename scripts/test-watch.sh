55d5ead6f5814039d9676916fb81ce79c09a5099
VdustR <VdustR@gmail.com>
2021-10-09 22:09:58 +0800

    compine test script

--- scripts/test-watch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-cd "$(dirname "$0")"
-cd ..
-deno test --importmap=import_map.json --unstable --watch .
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 cd "$(dirname "$0")"
 cd ..
-deno test --importmap=import_map.json --unstable .
+[[ $CI = true ]] && WATCH= || WATCH=--watch
+echo $WATCH
+deno test --importmap=import_map.json --unstable . $WATCH
